88b0d07d38fd8d62141bc76f4fe1e4e0a59879d0
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-07 22:10:41 -0800

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:qVtyyzERp2zpmOuh@bookstore-mern-stack.lkiu7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore-MERN-stack';
+
+// mongodb+srv://<username>:<password>@<cluster-url>/dbname?retryWrites=true&w=majority
---
+++ index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send('Welcome To MERN Stack Tutorial')
+});
+
+// Route for Save a New Book
+app.post('/books', async (req, res) => {
+    try {
+        if  (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+            ) {
+                return res.status(400).send({
+                    message: 'Send all required fields: title, author, publishYear',
+                });
+            }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+    
+        return res.status(201).send(book);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({ message: err.message })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
