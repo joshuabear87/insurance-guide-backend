ad267ea16c66fb120ef59ee6b44e67d1f9335cb4
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-15 21:16:28 -0700

    backend commit

--- index.js
+++ index.js
@@ -2,42 +2,27 @@ import express from 'express';
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }));
 
 app.get('/', (req, res) => {
     console.log(req)
     return res.status(234).send('Welcome To MERN Stack Tutorial')
 });
 
-// Route for Save a New Book
-app.post('/books', async (req, res) => {
-    try {
-        if  (
-            !req.body.title ||
-            !req.body.author ||
-            !req.body.publishYear
-            ) {
-                return res.status(400).send({
-                    message: 'Send all required fields: title, author, publishYear',
-                });
-            }
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-    
-        return res.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message })
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
