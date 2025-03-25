25e4a31c162bc3c4f304472542d78ed5535b17a9
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-25 13:33:20 -0700

    get rid of app.use('/')

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -8,25 +6,10 @@ import booksRoute from './routes/booksRoute.js';
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
 
-// API routes
 app.use('/books', booksRoute);
-
-// Serve static files if in production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/dist')));
-  
-  // Catch-all handler to serve index.html
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
-}
 
 mongoose
   .connect(mongoDBURL)
@@ -34,9 +17,9 @@ mongoose
     const port = process.env.PORT || PORT;
     console.log('App connected to database');
     app.listen(port, '0.0.0.0', () => {
-      console.log(`App is listening to port: ${port}`);
+      console.log(`App is listening on port: ${port}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.log('Failed to connect to MongoDB:', err);
   });
