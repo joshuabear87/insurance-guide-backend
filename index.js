206846db0875f795bda2efaa0e9d603a24569fb9
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-25 10:35:55 -0700

    binded our backend to the frontend

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+// const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 app.use(express.json());
 
 app.use(cors());
 
 // app.use(cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-// }));
-
-app.get('/', (req, res) => {
-    console.log(req)
-    return res.status(234).send('Welcome To MERN Stack Tutorial')
-});
+    //     origin: 'http://localhost:3000',
+    //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    //     allowedHeaders: ['Content-Type'],
+    // }));
 
 app.use('/books', booksRoute);
+    
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get('/*', (req, res) => {
+    // console.log(req)
+    // return res.status(234).send('Welcome To MERN Stack Tutorial')
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+});
+    
 
 mongoose
     .connect(mongoDBURL)
